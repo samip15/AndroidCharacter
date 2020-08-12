3f2cf1b93c70d43d0e9a0366af4311dac7ba6740
Samip <samipgnyawali15@gmail.com>
2020-08-12 22:21:48 +0545

    Added fragment and its communication with grid view

--- app/src/main/java/com/example/androidcharacter/ui/MainActivity.java
+++ app/src/main/java/com/example/androidcharacter/ui/MainActivity.java
@@ -2,15 +2,88 @@ package com.example.androidcharacter.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.androidcharacter.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListner{
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // pass bundle to android character activity
+        Bundle b = new Bundle();
+        b.putInt("headIndex",headIndex);
+        b.putInt("bodyIndex",bodyIndex);
+        b.putInt("legIndex",legIndex);
+
+        // attach to intent
+        final Intent intent = new Intent(this,AndroidCharacterActivity.class);
+        intent.putExtras(b);
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        Toast.makeText(this, "Position Clicked"+position, Toast.LENGTH_SHORT).show();
+
+        // get the body part number
+        //0-head
+        //1-body
+        //2-leg
+
+        int bodyPartNumber = position/12;
+        // list index
+        int listIndex = position-12*bodyPartNumber;
+
+        // selected image is placed on the body part fragment accordingly
+
+        switch (bodyPartNumber){
+            case 0:
+                headIndex = listIndex;
+                break;
+            case 1:
+                bodyIndex = listIndex;
+                break;
+            case  2:
+                legIndex = listIndex;
+                break;
+            default: break;
+        }
+
+        // pass bundle to android character activity
+        Bundle b = new Bundle();
+        b.putInt("headIndex",headIndex);
+        b.putInt("bodyIndex",bodyIndex);
+        b.putInt("legIndex",legIndex);
+
+        // attach to intent
+        final Intent intent = new Intent(this,AndroidCharacterActivity.class);
+        intent.putExtras(b);
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/androidcharacter/ui/MasterListFragment.java
+++ app/src/main/java/com/example/androidcharacter/ui/MasterListFragment.java
@@ -1,12 +1,15 @@
 package com.example.androidcharacter.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.androidcharacter.R;
@@ -16,8 +19,29 @@ import com.example.androidcharacter.data.AndroidImageAssets;
  * Created Master List Fragment
  */
 public class MasterListFragment extends Fragment {
+    // triggers a call back to the main activity
+    OnImageClickListner onImageClickListner;
 
+    // interface for image selected
+    public interface OnImageClickListner{
+        void onImageSelected(int position);
+    }
 
+    /**
+     * The Lifecycle Where activity and fragment connects
+     * @param context
+     */
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        // check if attach
+        try {
+            onImageClickListner = (OnImageClickListner) context;
+        }catch (ClassCastException e){
+            throw new ClassCastException(context.toString()+"is not implemented");
+        }
+    }
+    // necessary empty constructor
     public MasterListFragment() {
     }
 
@@ -29,6 +53,13 @@ public class MasterListFragment extends Fragment {
         GridView gridView = rootView.findViewById(R.id.imaged_grid_view);
         MasterListAdapter masterListAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
         gridView.setAdapter(masterListAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // get the position
+                onImageClickListner.onImageSelected(position);
+            }
+        });
         return rootView;
     }
 }
